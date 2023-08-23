89a0d3a0e8ad83d2c4fbd1028a0ba36fe0c96e9a
omotomiwa26 <omotomiwa26@gmail.com>
2023-08-23 23:05:26 +0000

    revised code to fix memory leaks and betty error

--- token_function.c
+++ token_function.c
@@ -5,10 +5,9 @@
  * @file: size of file read
  * Return: array of pointer to strings
  */
-char **command_tokens(char *cmd_input, char **cmd, ssize_t file)
+char **command_tokens(char *cmd_input, ssize_t file)
 {
-	char *tok, *cmd_input_cpy;
-	const char *delimiter = " \n";
+	char *tok, *cmd_input_cpy, **cmd;
 	int tok_len, tok_cnt = 0, i = 0;
 
 	cmd_input_cpy = malloc(sizeof(char) * (file + 2));
@@ -19,32 +18,30 @@ char **command_tokens(char *cmd_input, char **cmd, ssize_t file)
 		return (NULL);
 	}
 	_strcpy(cmd_input_cpy, cmd_input);
-	tok = _strtok(cmd_input, delimiter);
+	tok = _strtok(cmd_input, " \n");
 
 	while (tok)
 	{
 		tok_cnt++;
-		/*free(tok);*/
-		tok = _strtok(NULL, delimiter);
+		tok = _strtok(NULL, " \n");
 	}
 	tok_cnt++;
 
 	cmd = malloc(sizeof(char *) * tok_cnt);
 	if (cmd == NULL)
 	{
-	perror("Error: Memory Allocation Failed");
-	free(cmd);
-	return (NULL);
+		perror("Error: Memory Allocation Failed");
+		free(cmd);
+		return (NULL);
 	}
-	tok = _strtok(cmd_input_cpy, delimiter);
+	tok = _strtok(cmd_input_cpy, " \n");
 	for (i = 0; tok != NULL; i++)
 	{
 		tok_len = _strlen(tok);
 		cmd[i] = malloc(sizeof(char) * tok_len + 1);
 		cmd[i][tok_len] = '\0';
 		_strcpy(cmd[i], tok);
-		/*free(tok);*/
-		tok = _strtok(NULL, delimiter);
+		tok = _strtok(NULL, " \n");
 	}
 	free(tok);
 	cmd[i] = NULL;
