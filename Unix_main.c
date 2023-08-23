1e77389679329d392659dc234602f8d175940e82
omotomiwa26 <omotomiwa26@gmail.com>
2023-08-23 23:00:23 +0000

    fixed betty error and revised code

--- Unix_main.c
+++ Unix_main.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * main - program executes the Unix shell command line.
+ * main - program executes the Unix Simple_shell command line.
  * @argc: integer of argument count.
  * @argv: array pointer to strings of C.L arguments.
  * Return: 0.
@@ -9,25 +9,24 @@
 
 int main(int argc, char *argv[])
 {
-	char *unix_input;/***input_arg;*/
+	char *unix_input;
 	ssize_t file_check;
-	int /*exe_env,*/ exe_stat = 0;
+	int exe_stat = 0;
 
 	(void)argc;
-	/*(void)argv;*/
 
 	while (1)
 	{
 		unix_input = NULL, file_check = 0;
 		unix_input = shell_prompt(&file_check);
-		argv = command_tokens(unix_input, argv, file_check);
+		argv = command_tokens(unix_input, file_check);
 		if (argv[0] == NULL)
 		{
 			free(unix_input);
 			free(argv);
 			continue;
 		}
-		if (_strncmp(argv[0], "exit", 4) == 0)
+		if (_strncmp(argv[0], "exit", _strlen(argv[0])) == 0)
 		{
 			free(unix_input);
 			exit_prompt(argv, exe_stat);
@@ -42,7 +41,7 @@ int main(int argc, char *argv[])
 		if (execute_env(argv) != 0)
 		{
 			free(unix_input);
-		free_array_vectors(argv);
+			free_array_vectors(argv);
 		}
 	}
 	return (0);
